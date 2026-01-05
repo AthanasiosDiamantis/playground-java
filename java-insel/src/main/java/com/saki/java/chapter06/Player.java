c8f82fc3054b9033ea42b6fbe35b3045db4ff025
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-01-05 16:53:49 +0100

    first commit class Hogwurtz and refactoring in class Player

--- java-insel/src/main/java/com/saki/java/chapter06/Player.java
+++ java-insel/src/main/java/com/saki/java/chapter06/Player.java
@@ -1,7 +1,22 @@
 package com.saki.java.chapter06;
 
 public class Player {
-    String name;
-    String item;
+    String name = "";
+    String item = "";
+
+
+    public void carry(String newItem){
+        if(newItem != null && !newItem.trim().isEmpty()) {
+            item += newItem;
+        }
+    }
+
+    public boolean doesCarryAnything() {
+        return !item.isEmpty();
+    }
+
+    public void dropAll() {
+        item = "";
+    }
 
 }
