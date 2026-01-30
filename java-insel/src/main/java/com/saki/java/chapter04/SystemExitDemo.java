9451c76ca65d5ac93c1ec71a02b4f16b41cc2ce8
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-01-30 12:41:03 +0100

    first commit

---
+++ java-insel/src/main/java/com/saki/java/chapter04/SystemExitDemo.java
@@ -0,0 +1,8 @@
+package com.saki.java.chapter04;
+
+public class SystemExitDemo {
+    public static void main(String[] args) {
+        System.out.println("Some milliseconds before death...");
+        System.exit(42);
+    }
+}
