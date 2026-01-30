ebe71bcc10fe974ca50f45a482371921582c87a7
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-01-30 13:19:12 +0100

    add comments

--- java-insel/src/main/java/com/saki/java/chapter04/SystemExitDemo.java
+++ java-insel/src/main/java/com/saki/java/chapter04/SystemExitDemo.java
@@ -1,5 +1,11 @@
 package com.saki.java.chapter04;
 
+/**
+ * first I compiled the class with "mvn compile"
+ * after I wrote the SHELL-skript showreturn.sh and put it into the root-folder
+ * of target.
+ * then I startet the skript with ./showreturn.sh in the terminal
+ */
 public class SystemExitDemo {
     public static void main(String[] args) {
         System.out.println("Some milliseconds before death...");
