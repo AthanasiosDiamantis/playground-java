1bcb66d37dc72a9f7132765cca8f530a20c8ae52
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-03-02 13:13:41 +0100

    refactoring

--- java-insel/src/main/java/com/saki/javainsel17/chapter20/ListAllLines.java
+++ java-insel/src/main/java/com/saki/javainsel17/chapter20/ListAllLines.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
 public class ListAllLines {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        URI uri = ListAllLines.class.getResource("/lirics.txt").toURI();
+        URI uri = ListAllLines.class.getResource("/lyrics.txt").toURI();
         Path p = Paths.get(uri);
 
         System.out.printf("File '%s' with size of %d byte(s) with lines:%n",p.getFileName(), Files.size(p));
