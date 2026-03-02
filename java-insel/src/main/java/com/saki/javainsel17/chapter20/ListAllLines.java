a85a68c08607a87922ba911168de64de5cd9895e
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-03-02 13:09:35 +0100

    add new code for java insel chapter20 files and streams

---
+++ java-insel/src/main/java/com/saki/javainsel17/chapter20/ListAllLines.java
@@ -0,0 +1,24 @@
+package com.saki.javainsel17.chapter20;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ListAllLines {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        URI uri = ListAllLines.class.getResource("/lirics.txt").toURI();
+        Path p = Paths.get(uri);
+
+        System.out.printf("File '%s' with size of %d byte(s) with lines:%n",p.getFileName(), Files.size(p));
+
+        int lineCnt = 1;
+        for( String line : Files.readAllLines(p) ) {
+            System.out.println(lineCnt++ +": " + line);
+        }
+
+    }
+}
