62c8429444c0919aa80a9867d86852a3abc0c5c1
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-03-06 17:25:26 +0100

    add new example code from javaSE9 network programming

---
+++ java-insel/src/main/java/com/saki/javainselse9/chapter11/MyDNS.java
@@ -0,0 +1,31 @@
+package com.saki.javainselse9.chapter11;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MyDNS {
+
+    public static void main(String[] args) throws IOException {
+        InetAddress inet = InetAddress.getByName("www.tutego.de");
+        System.out.println(inet.getCanonicalHostName());
+        System.out.println(inet.getHostAddress());
+        System.out.println(inet);
+
+        inet = InetAddress.getByName("193.99.144.71");
+        System.out.println(inet.getHostName());
+
+        System.out.println(InetAddress.getByName("136.243.210.170").isReachable(2000));
+//        System.out.println(InetAddress.getByName("100.100.100.100").isReachable(2000));
+
+        // Check the local host address
+        System.out.println("Host Name/Address: " + InetAddress.getLocalHost());
+
+        // Find out if your local machine has more than one ip-address
+        String localHost = InetAddress.getLocalHost().getHostName();
+        for(InetAddress inetAddress : InetAddress.getAllByName(localHost)) {
+            System.out.println(inetAddress);
+        }
+
+    }
+}
