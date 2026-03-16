f3401bca5939ef64d70cea840b8ec44a0010d701
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-03-16 14:17:05 +0100

    add new example code from javaSE9 swing GUI

---
+++ java-insel/src/main/java/com/saki/javase9/chapter17/layout/FlowLayoutDemo.java
@@ -0,0 +1,24 @@
+package com.saki.javase9.chapter17.layout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutDemo {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new FlowLayout());
+
+        JComboBox<String> choice = new JComboBox<>();
+        choice.addItem("Mike: Mein Gott, Walter");
+        choice.addItem("Sweet: Co Co");
+
+        f.add(choice);
+        f.add( new JButton(
+                new ImageIcon( FlowLayoutDemo.class.getResource("/images/play.png"))
+        ));
+        f.pack();
+        f.setVisible(true);
+    }
+}
---
+++ java-insel/src/main/java/com/saki/javase9/chapter17/swing/JTabbedPaneDemo.java
@@ -0,0 +1,20 @@
+package com.saki.javase9.chapter17.swing;
+
+import javax.swing.*;
+
+public class JTabbedPaneDemo {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Reiter 1",new JButton("Reiter 1"));
+        tabbedPane.addTab("Reiter 2",new JTextArea());
+//        tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
+
+        frame.add(tabbedPane);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
