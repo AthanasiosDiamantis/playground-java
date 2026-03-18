5836d9e486f6c60bad9ee56c87a6ae5e811b31ce
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-03-18 15:04:32 +0100

    add new example code from javaSE9 mysql and docker

--- java-insel/src/main/java/com/saki/javase9/chapter16/FirstSqlAccess.java
+++ java-insel/src/main/java/com/saki/javase9/chapter16/FirstSqlAccess.java
@@ -1,30 +1,32 @@
-package com.saki.javainsel17.chapter21.withchatgpt;
+package com.saki.javase9.chapter16;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class ReadUsersExample {
+public class FirstSqlAccess {
+
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/javademo?allowPublicKeyRetrieval=true&useSSL=false";
         String user = "javauser";
         String password = "javapass123";
 
-        String sql = "SELECT id, name, email FROM users";
+        String query = "SELECT * FROM users";
 
         try (Connection con = DriverManager.getConnection(url, user, password);
              Statement stmt = con.createStatement();
-             ResultSet resultSet = stmt.executeQuery(sql)) {
-            while (resultSet.next()) {
+             ResultSet resultSet = stmt.executeQuery(query)) {
+            while(resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
                 String email = resultSet.getString("email");
-                System.out.printf("id: %d, name: %s, email: %s%n", id, name, email);
+                System.out.printf("%d, %s, %s, %n", id, name, email);
             }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
